b837f4638823224aa19cd77b24e0ea7ec955bc06
zol <zolppy.me@gmail.com>
2026-04-01 10:52:24 -0300

    refactor: tratamento de erros

--- app/utils/dir.py
+++ app/utils/dir.py
@@ -1,13 +1,19 @@
-from typing import Self
 from pathlib import Path
 
 
 class Dir:
-    def __init__(self: Self, path: Path | str) -> None:
+    def __init__(self, path: Path | str) -> None:
         self.path = Path(path)
 
-    def is_empty(self: Self) -> bool:
-        return not any(self.path.iterdir())
+    def exists_and_is_dir(self) -> bool:
+        return self.path.exists() and self.path.is_dir()
 
-    def is_dir(self: Self) -> bool:
-        return self.path.is_dir()
+    def ensure_is_dir(self) -> None:
+        if not self.path.exists():
+            raise FileNotFoundError(f"Caminho inexistente: {self.path}")
+        if not self.path.is_dir():
+            raise NotADirectoryError(f"Não é um diretório: {self.path}")
+
+    def is_empty(self) -> bool:
+        self.ensure_is_dir()
+        return not any(self.path.iterdir())
