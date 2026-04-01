d045d54345f196fa709c3bbbcd0c5813df8e81fa
zol <zolppy.me@gmail.com>
2026-04-01 10:26:54 -0300

    fix: carregamento de banco vetorial existente

--- app/utils/rag.py
+++ app/utils/rag.py
@@ -1,4 +1,4 @@
-from .dir import Dir
+from . import dir
 from pathlib import Path
 from typing import Self, List, Any
 from langchain_chroma import Chroma
@@ -11,19 +11,29 @@ class Rag:
         self: Self,
         documents: List[Document],
         persist_directory: Path | str,
-        **chroma_kwars: Any,
+        **chroma_kwargs: Any,
     ) -> None:
         self.embedding = HuggingFaceEmbeddings()
         self.documents = documents
-        self.persist_directory = persist_directory
+        self.persist_directory = Path(persist_directory)
 
-        dir_persist_directory = Dir(persist_directory)
+        # Garante que persist_directory exista
+        self.persist_directory.mkdir(parents=True, exist_ok=True)
 
-        # Evita recriação do mesmo banco vetorial toda vez que um novo objeto for instanciado
-        if dir_persist_directory.is_dir() and dir_persist_directory.is_empty():
+        dir_persist_directory = dir.Dir(self.persist_directory)
+
+        # Se o diretório está vazio, cria o banco vetorial a partir dos documentos
+        if dir_persist_directory.is_empty():
             self.vector_store = Chroma.from_documents(  # type: ignore
                 documents=documents,
                 embedding=self.embedding,
-                persist_directory=str(persist_directory),
-                **chroma_kwars,
+                persist_directory=str(self.persist_directory),
+                **chroma_kwargs,
+            )
+        else:
+            # Carrega banco vetorial existente
+            self.vector_store = Chroma(
+                persist_directory=str(self.persist_directory),
+                embedding_function=self.embedding,
+                **chroma_kwargs,
             )
