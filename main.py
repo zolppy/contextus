7ba141f20b37cb50f822a87be5c192c8375cb2d0
zol <zolppy.me@gmail.com>
2026-03-31 23:02:37 -0300

    feat: split documents

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from app import PDFDocumentProcessor
 if __name__ == "__main__":
     processor = PDFDocumentProcessor(documents_path=Path("app/documents"))
     documents = processor.load_documents()
-    print(documents)
+    split_documents = processor.split_documents(documents)
+    print(split_documents)
